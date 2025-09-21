40bd91f2913faed87c958531e2332e853dd13877
open-meadow <rohan.varughese200@gmail.com>
2025-09-21 14:58:26 +0530

    Initial Commit

---
+++ self_quiz_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  self_quiz_app
+//
+//  Created by open-meadow on 2025-09-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
